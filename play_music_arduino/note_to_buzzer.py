947244ee184f23b8e23980e5cdfc90e179ecc312
ltkdt <kienlt2306@gmail.com>
2024-08-22 21:37:20 +0700

    now it can work with sheets that use two hand, still advise against this practice tho

--- play_music_arduino/note_to_buzzer.py
+++ play_music_arduino/note_to_buzzer.py
@@ -5,7 +5,11 @@ fileName = "note.txt"
 '''
 I don't claim copyright to the original source
 Check out: https://pianoletternotes.blogspot.com/ . They help piano beginners a lot with simplified version of popular songs.
-For this script, I'm playing Bad Apple: https://pianoletternotes.blogspot.com/2018/03/bad-apple-teppei-okada.html
+For this script, I'm playing Bad Apple: https://pianoletternotes.blogspot.com/2018/03/bad-apple-teppei-okada.html 
+
+Another warning:
+Right now, you can use sheets on the website that use two hands. However I discourage you to do so. The reason behind that is that there are many notes that overlap, 
+as a result, the music can not be heard clearly or create the intended sound
 '''
 
 note_dictionary = {
@@ -29,12 +33,13 @@ data = Path(fileName).read_text()
 
 line_count = 0
 
-list_0 = [segment for segment in data.split("\n\n")]
-
-#print(list_0[0].split("\n"))
-
-
-def handle_one_line(line):
+# The below line is meant to check if the formatting from text to different lines work correctly:
+# list_0 = [segment for segment in data.split("\n\n")]
+ 
+# Setting two_hand will delete the "RH" and "LH" part
+def handle_one_line(line, two_hand = False):
+    if two_hand:
+        line = line[3:]
     output = []
     octave = line[0]
     for i in range(2, len(line) - 1 ):
@@ -45,8 +50,10 @@ def handle_one_line(line):
     return output
 
 
-def handle_multiple_line(segment):
+def handle_multiple_line(segment, two_hand = False):
     lines = segment.split("\n")
+    if two_hand:
+        lines = [line[3:] for line in lines]
     output = handle_one_line(lines[0])
 
     for line in lines:
@@ -54,16 +61,31 @@ def handle_multiple_line(segment):
         for i in range(2, len(line) - 1):
             if output[i-2] == "0" and line[i] != "-":
                 output[i-2] = note_dictionary[line[i]] + octave
+            '''
+            If the main melody lies in the lower lines, keep this if clause. 
+            Cut this if it is not the case. Normally this is not the case for two-handed tutorial since lines starting with "RH" 
+            are for the melody and those with RH are meant for bass
+            '''
+            # if output[i-2] != "0" and line[i] != "-":                       
+            #    output[i-2] = note_dictionary[line[i]] + octave
     return output
 
 buzzer_final = []
 
+
 for segment in data.split("\n\n"):
     if segment.count("\n") == 0:
-        buzzer_final.extend(handle_one_line(segment))
-
+        if segment[0][0] == "L" or segment[0] == "R":
+            buzzer_final.extend(handle_one_line(segment, True))
+        else:
+            buzzer_final.extend(handle_one_line(segment))
     else:
-        buzzer_final.extend(handle_multiple_line(segment))
+        if segment[0][0] == "L" or segment[0] == "R":
+            buzzer_final.extend(handle_multiple_line(segment, True))
+        else:
+            buzzer_final.extend(handle_multiple_line(segment))
 
 print(*buzzer_final, sep=', ')
 
+# If you need to count the number of notes
+print(len(buzzer_final))
